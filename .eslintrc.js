66cb30d23abb96f292904aa446e4354e3f4e40df
gabyNicliz <gabynicliz93@gmail.com>
2021-01-20 12:25:24 -0300

    Deployment pipeline: lint fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,7 @@ module.exports = {
       "error", { "before": true, "after": true }
     ],
     "no-console": "error",
-    "react/prop-types": 0
+    "react/prop-types": 0,
+    "linebreak-style": 0
   }
 }
